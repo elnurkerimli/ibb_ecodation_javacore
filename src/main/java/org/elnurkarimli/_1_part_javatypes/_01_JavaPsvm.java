e0def63bfad58d674c2728e673b434a40d036135
Elnur Karimli <elnurkerimli2@gmail.com>
2025-04-11 04:36:20 +0300

    primitive

---
+++ src/main/java/org/elnurkarimli/_1_part_javatypes/_01_JavaPsvm.java
@@ -0,0 +1,14 @@
+package org.elnurkarimli._1_part_javatypes;
+
+// single comment
+/*
+Multiple
+*/
+public class _01_JavaPsvm {
+    // psvm => tab
+    public static void main(String[] args) {
+// Çıktı
+//sout => tab
+        System.out.println("Mr. Elnur Karimli");
+    } // end psvm
+} //end class
